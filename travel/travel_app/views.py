fd708141f48c4ecd64e4070cf57bcfb7925e3787
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-21 10:12:43 +0200

    site setup

--- travel/travel_app/views.py
+++ travel/travel_app/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
+def index(request):
+    dest1=Destination()
+    dest1.name='Brasov'
+    dest1.desc='Brasov is the best'
+    dest1.price=300
+    dest1.img='destination_1.jpg'
+    dest1.offer=False
 
-def home(request):
-    return render(request, 'index.html', {})
+    dest2=Destination()
+    dest2.name='Sinaia'
+    dest2.desc='Sinaia rocks!!!'
+    dest2.price=150
+    dest2.img='destination_2.jpg'
+    dest2.offer=True
 
-def add(request):
-    num1=int(request.GET['num1'])
-    num2=int(request.GET['num2'])
-    res=num1+num2
-    return render(request, 'result.html', {'result':res})
+    dest3=Destination()
+    dest3.name='Sibiu'
+    dest3.desc='I love Sibiu!'
+    dest3.price=450
+    dest3.img='destination_3.jpg'
+    dest3.offer=False
+
+    destinations=[dest1, dest2, dest3]
+    return render(request, 'index.html', {'price':700, 'destinations':destinations})
