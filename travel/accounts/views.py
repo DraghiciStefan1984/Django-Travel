00e5d17958a0355513eb2ede64fd7c507aa49dc9
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-23 07:28:20 +0200

    register start

--- travel/accounts/views.py
+++ travel/accounts/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html', {})
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        username=request.POST['username']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        email=request.POST['email']
+        user=User.objects.create_user(username=username, email=email, password=password1, pas)
+
+    return render(request, 'register.html', {})
